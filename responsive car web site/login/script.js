e4fff1360b089255c0605647959b990825e479c1
courage <tezikitty@gmail.com>
2025-08-29 02:16:47 -0700

    service section
    here i did the service section using css, html,and boostrap

--- responsive car web site/login/script.js
+++ responsive car web site/login/script.js
@@ -7,3 +7,17 @@ function showLogin() {
   document.getElementById("signupForm").classList.add("hidden");
   document.getElementById("loginForm").classList.remove("hidden");
 }
+
+function loginUser() {
+      const user = document.getElementById("username").value;
+      const pass = document.getElementById("password").value;
+
+      // Just a simple check for demo
+      if(user === "admin@gmail.com" && pass === "1234"){
+        window.location.href = "responsive car web site\index.html"; // Go to home page
+        return false;
+      } else {
+        alert("Invalid login details!");
+        return false;
+      }
+}
