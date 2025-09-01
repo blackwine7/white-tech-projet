dc7dcf18e62b6c81a9e80c93e1eb6f60c9cafc8d
blackwine7 <dacostagic@gmail.com>
2025-09-01 01:54:31 +0100

    changes
    some adjustments

--- responsive car web site/index.js
+++ responsive car web site/index.js
@@ -1,1 +1,22 @@
-// slider javascript code 
+// button javascript code
+let hideContent = document.querySelector('.hideContent');
+let hidecontent = document.querySelector('.hidecontent');
+let button = document.querySelector('.fa-bars');
+let butt = document.querySelector('.fa-times');
+let isShow = true;
+
+// a function to hide and show the section target  on click
+function toggleButton() {
+        hideContent.classList.toggle('hide');
+        hidecontent.classList.toggle('hide');
+        butt.classList.toggle('hide');
+        button.classList.remove('hide');
+   
+}
+function toggleButton2() {
+        hideContent.classList.toggle('hide');
+        hidecontent.classList.toggle('hide');
+        button.classList.toggle('hide');
+        butt.classList.remove('hide');
+}
+
